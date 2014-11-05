7888177bc4a2a7aeba383ab4d9a07d2efa3d104b
junlincao <cjltgs@163.com>
2014-11-05 10:10:05 +0800

    test_commit

--- src/com/caiyi/ui/SwitchTitle.java
+++ src/com/caiyi/ui/SwitchTitle.java
@@ -2,113 +2,175 @@ package com.caiyi.ui;
 
 import java.util.ArrayList;
 
-import com.caiyi.testfootball.R;
-
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.os.Build;
+import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.Gravity;
-import android.view.TextureView;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class SwitchTitle extends HorizontalScrollView {
+import com.caiyi.testfootball.R;
 
-	private int mTxtActiveColor;
-	private int mTxtDisableColor;
-	private int mTabLineHeight;
-	private int mTabLineColor;
-	private int mTxtSize;
-	private int mTabPadding;
-	private int mExtraPadding = 0;
+public class SwitchTitle extends HorizontalScrollView implements OnClickListener {
 
-	private TabContainer mTabContainer;
+    private int mTxtActiveColor;
+    private int mTxtDisableColor;
+    private int mTabLineHeight;
+    private int mTabLineColor;
+    private int mTxtSize;
+    private int mTabPadding;
+    private int mExtraPadding = 0;
 
-	@TargetApi(Build.VERSION_CODES.L)
-	public SwitchTitle(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-		super(context, attrs, defStyleAttr, defStyleRes);
-		init(context, attrs, defStyleAttr);
-	}
+    private TabContainer mTabContainer;
 
-	public SwitchTitle(Context context, AttributeSet attrs, int defStyleAttr) {
-		super(context, attrs, defStyleAttr);
-		init(context, attrs, defStyleAttr);
-	}
+    private static final String TAG = "SwitchTitle";
 
-	public SwitchTitle(Context context, AttributeSet attrs) {
-		super(context, attrs, 0);
-	}
+    @TargetApi(Build.VERSION_CODES.L)
+    public SwitchTitle(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context, attrs, defStyleAttr);
+    }
 
-	public SwitchTitle(Context context) {
-		this(context, null);
-	}
+    public SwitchTitle(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs, defStyleAttr);
+    }
 
-	private void init(Context context, AttributeSet attrs, int defStyleAttr) {
-		Resources res = context.getResources();
-		TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.switchTitle, defStyleAttr, 0);
+    public SwitchTitle(Context context, AttributeSet attrs) {
+        super(context, attrs, 0);
+    }
 
-		mTxtActiveColor = a.getColor(R.attr.tabTxtActiveColor, res.getColor(R.color.switchTitleTxtActive));
-		mTxtDisableColor = a.getColor(R.attr.tabTxtDisableColor, res.getColor(R.color.switchTitleTxtDisable));
-		mTabLineHeight = a.getDimensionPixelSize(R.attr.tabLineHeight,
-				res.getDimensionPixelSize(R.dimen.switchTitleLineHeight));
-		mTabLineColor = a.getColor(R.attr.tabLineColor, res.getColor(R.color.switchTitleline));
-		mTxtSize = a.getDimensionPixelSize(R.attr.tabTxtSize, res.getDimensionPixelSize(R.dimen.switchTitleTxtSize));
-		mTxtSize = a.getDimensionPixelSize(R.attr.tabPadding, res.getDimensionPixelSize(R.dimen.switchTitleTabPadding));
-		a.recycle();
-		mTabContainer = new TabContainer(context);
+    public SwitchTitle(Context context) {
+        this(context, null);
+    }
 
-		addView(mTabContainer, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
-				ViewGroup.LayoutParams.MATCH_PARENT));
-	}
+    private void init(Context context, AttributeSet attrs, int defStyleAttr) {
+        Resources res = context.getResources();
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.switchTitle, defStyleAttr, 0);
 
-	/** 设置标题 **/
-	public void setTitle(ArrayList<CharSequence> titles) {
-		mTabContainer.removeAllViews();
-		for (CharSequence c : titles) {
-			TabText tt = new TabText(getContext());
-			tt.setText(c);
-			mTabContainer.addView(tt, LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT);
-		}
-	}
+        mTxtActiveColor = a.getColor(R.attr.tabTxtActiveColor, res.getColor(R.color.switchTitleTxtActive));
+        mTxtDisableColor = a.getColor(R.attr.tabTxtDisableColor, res.getColor(R.color.switchTitleTxtDisable));
+        mTabLineHeight = a.getDimensionPixelSize(R.attr.tabLineHeight,
+                res.getDimensionPixelSize(R.dimen.switchTitleLineHeight));
+        mTabLineColor = a.getColor(R.attr.tabLineColor, res.getColor(R.color.switchTitleline));
+        mTxtSize = a.getDimensionPixelSize(R.attr.tabTxtSize, res.getDimensionPixelSize(R.dimen.switchTitleTxtSize));
+        mTxtSize = a.getDimensionPixelSize(R.attr.tabPadding, res.getDimensionPixelSize(R.dimen.switchTitleTabPadding));
+        a.recycle();
+        mTabContainer = new TabContainer(context);
 
-	private class TabContainer extends LinearLayout {
+        addView(mTabContainer, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+    }
 
-		public TabContainer(Context context) {
-			super(context);
-		}
+    private ViewPager mViewPager;
+    private OnPageChangeListener mOnPageChange;
 
-		@Override
-		protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-			// TODO Auto-generated method stub
-			super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-		}
+    public void setOnPageChangeListener(OnPageChangeListener lis) {
+        mOnPageChange = lis;
+    }
 
-		@Override
-		protected void measureChildren(int widthMeasureSpec, int heightMeasureSpec) {
-			// TODO Auto-generated method stub
-			super.measureChildren(widthMeasureSpec, heightMeasureSpec);
-		}
-	}
+    public interface OnPageChangeListener {
+        void onPageSelected(int arg0);
 
-	/** tab TextView **/
-	private class TabText extends TextView {
-		public TabText(Context context) {
-			super(context);
-			init(context);
-		}
+        void onPageScrolled(int arg0, float arg1, int arg2);
 
-		private void init(Context context) {
-			setTextSize(mTxtSize);
-			setTextColor(mTxtDisableColor);
-			setGravity(Gravity.CENTER);
-			setPadding(mTabPadding + mExtraPadding, 0, mTabPadding + mExtraPadding, 0);
-		}
+        void onPageScrollStateChanged(int arg0);
+    }
 
-	}
+    /** 设置标题 **/
+    public void setTitle(ArrayList<CharSequence> titles, ViewPager vp) {
+        if (mViewPager != vp) {
+            mViewPager.setOnPageChangeListener(null);
+            mViewPager = vp;
+        }
+        mTabContainer.removeAllViews();
+        for (CharSequence c : titles) {
+            TabText tt = new TabText(getContext());
+            tt.setText(c);
+            mTabContainer.addView(tt, LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT);
+        }
+
+        mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageSelected(int arg0) {
+
+                if (mOnPageChange != null) {
+                    mOnPageChange.onPageSelected(arg0);
+                }
+                Log.d(TAG, "onPageSelected->arg0=" + arg0);
+            }
+
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+                if (mOnPageChange != null) {
+                    mOnPageChange.onPageScrolled(arg0, arg1, arg2);
+                }
+
+                Log.d(TAG, "onPageScrolled->arg0=" + arg0 + "; arg1=" + arg1 + "; arg2=" + arg2);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+
+                if (mOnPageChange != null) {
+                    mOnPageChange.onPageScrollStateChanged(arg0);
+                }
+                Log.d(TAG, "onPageScrollStateChanged->arg0=" + arg0);
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+
+    }
+
+    private class TabContainer extends LinearLayout {
+        public TabContainer(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected void measureChildren(int widthMeasureSpec, int heightMeasureSpec) {
+            super.measureChildren(widthMeasureSpec, heightMeasureSpec);
+
+            int sumChildWidth = 0;
+            for (int i = 0; i < getChildCount(); i++) {
+                sumChildWidth += getChildAt(i).getMeasuredWidth();
+            }
+
+            if (getWidth() > sumChildWidth) {
+                mExtraPadding = (getWidth() - sumChildWidth) / (2 * getChildCount());
+
+                for (int i = 0; i < getChildCount(); i++) {
+                    getChildAt(i).setPadding(mTabPadding + mExtraPadding, 0, mTabPadding + mExtraPadding, 0);
+                }
+            }
+        }
+    }
+
+    /** tab TextView **/
+    private class TabText extends TextView {
+        public TabText(Context context) {
+            super(context);
+            init(context);
+        }
+
+        private void init(Context context) {
+            setTextSize(mTxtSize);
+            setTextColor(mTxtDisableColor);
+            setGravity(Gravity.CENTER);
+            setPadding(mTabPadding + mExtraPadding, 0, mTabPadding + mExtraPadding, 0);
+        }
+    }
 }
