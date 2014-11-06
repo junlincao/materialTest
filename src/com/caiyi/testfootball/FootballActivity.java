530928664e73e376d26cfc281fca46799528d4d1
junlincao <cjltgs@163.com>
2014-11-07 00:50:23 +0800

    回归使用computeVerticalScrollOffset 考虑继承ListView、ScrollView等控件，自己写...

--- src/com/caiyi/testfootball/FootballActivity.java
+++ src/com/caiyi/testfootball/FootballActivity.java
@@ -26,182 +26,177 @@ import com.caiyi.ui.SwitchTitle.OnPageChangeListener;
 
 public class FootballActivity extends ActionBarActivity {
 
-    static final String TAG = "FootballActivity";
-    private MulTabActionBar mTabActionbar;
+	static final String TAG = "FootballActivity";
+	private MulTabActionBar mTabActionbar;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_football);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_football);
 
-        mTabActionbar = (MulTabActionBar) findViewById(R.id.mtab_title);
-        setSupportActionBar(mTabActionbar.getToolBar());
-        SwitchTitle st = mTabActionbar.getSwitchTitle();
-        ViewPager vp = (ViewPager) findViewById(R.id.vp_pager);
+		mTabActionbar = (MulTabActionBar) findViewById(R.id.mtab_title);
+		setSupportActionBar(mTabActionbar.getToolBar());
+		SwitchTitle st = mTabActionbar.getSwitchTitle();
+		ViewPager vp = (ViewPager) findViewById(R.id.vp_pager);
 
-        ActionBar ab = getSupportActionBar();
-        ab.setDisplayHomeAsUpEnabled(true);
+		ActionBar ab = getSupportActionBar();
+		ab.setDisplayHomeAsUpEnabled(true);
 
-        ArrayList<CharSequence> titles = new ArrayList<CharSequence>();
-        titles.add("page");
-        titles.add("咖啡");
-        titles.add("白咖啡");
-        titles.add("原味");
-        titles.add("怡宝");
-        titles.add("马来西亚");
-        titles.add("40g");
-        titles.add("生产日期");
-        titles.add("三合一");
+		ArrayList<CharSequence> titles = new ArrayList<CharSequence>();
+		titles.add("page");
+		titles.add("咖啡");
+		titles.add("白咖啡");
+		titles.add("原味");
+		titles.add("怡宝");
+		titles.add("马来西亚");
+		titles.add("40g");
+		titles.add("生产日期");
+		titles.add("三合一");
 
-        final MPagerAdapter mPageAdapter = new MPagerAdapter(titles);
-        vp.setAdapter(mPageAdapter);
-        st.setTitle(titles, vp);
+		final MPagerAdapter mPageAdapter = new MPagerAdapter(titles);
+		vp.setAdapter(mPageAdapter);
+		st.setTitle(titles, vp);
 
-        mTabActionbar.setCanHideOnScroll(true);
-        st.setOnPageChangeListener(new OnPageChangeListener() {
-            @Override
-            public void onPageSelected(int arg0) {
-                ListView lv = (ListView) mPageAdapter.getmViews(arg0);
-                mTabActionbar.setTouchHideView(lv);
-//                try {
-//                    int maxY = ListViewHelper.getScrollRange(lv);
-//                    mTabActionbar.setCanHideOnScroll(maxY);
-//                    int y = ListViewHelper.getScrollY(lv);
-//                    mTabActionbar.onPageChanged(y);
-//                } catch (NotSupportException e) {
-//                    Log.e(TAG, e.getMessage(), e);
-//                }
-            }
+		st.setOnPageChangeListener(new OnPageChangeListener() {
+			@Override
+			public void onPageSelected(int arg0) {
+				ListView lv = (ListView) mPageAdapter.getmViews(arg0);
+				try {
+					mTabActionbar.setCanHideOnScroll(ListViewHelper.getScrollRange(lv) - lv.getHeight()
+							- lv.getPaddingTop());
+					mTabActionbar.onPageChanged(ListViewHelper.getScrollY(lv));
+				} catch (NotSupportException e) {
+					Log.e(TAG, e.getMessage(), e);
+				}
+			}
 
-            @Override
-            public void onPageScrolled(int arg0, float arg1, int arg2) {
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
 
-            }
+			}
 
-            @Override
-            public void onPageScrollStateChanged(int arg0) {
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
 
-            }
-        });
+			}
+		});
+	}
 
-//        mTabActionbar.setTouchHideView(vp);
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			finish();
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-        case android.R.id.home:
-            finish();
-            break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	private ListView getPageListView(CharSequence title, int listCount) {
+		ListView lv = new ListView(this);
+		lv.setPadding(0, getResources().getDimensionPixelSize(R.dimen.actionbar_height), 0, 0);
+		lv.setClipToPadding(false);
+		lv.setClipChildren(false);
+		lv.setAdapter(new MTmpListAdapter(title, listCount));
+		lv.setOnScrollListener(new OnScrollListener() {
+			@Override
+			public void onScrollStateChanged(AbsListView view, int scrollState) {
+				if (scrollState == OnScrollListener.SCROLL_STATE_IDLE) {
+					mTabActionbar.finishScroll();
+				}
+			}
 
-    private ListView getPageListView(CharSequence title, int listCount) {
-        ListView lv = new ListView(this);
-//        lv.setPadding(0, getResources().getDimensionPixelSize(R.dimen.actionbar_height), 0, 0);
-//        lv.setClipToPadding(false);
-        lv.setAdapter(new MTmpListAdapter(title, listCount));
-//        lv.setOnScrollListener(new OnScrollListener() {
-//            @Override
-//            public void onScrollStateChanged(AbsListView view, int scrollState) {
-//                if (scrollState == OnScrollListener.SCROLL_STATE_IDLE) {
-//                    mTabActionbar.finishScroll();
-//                }
-//            }
-//
-//            @Override
-//            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount,
-//                    int totalItemCount) {
-//                try {
-//                    int y = ListViewHelper.getScrollY(view);
-//
-//                    Log.d(TAG, "OnScrollListener scrollPos=" + y);
-//
-//                    mTabActionbar.onScroll(y);
-//                } catch (NotSupportException e) {
-//                    Log.e(TAG, e.getMessage(), e);
-//                }
-//            }
-//        });
-        return lv;
-    }
+			@Override
+			public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+				try {
+					int y = ListViewHelper.getScrollY(view);
 
-    private class MPagerAdapter extends PagerAdapter {
-        private ArrayList<CharSequence> mTitles = new ArrayList<CharSequence>(0);
-        private SparseArray<View> mViews;
+//					Log.d(TAG, "OnScrollListener scrollPos=" + y);
 
-        public MPagerAdapter(ArrayList<CharSequence> titles) {
-            mTitles = titles;
-            mViews = new SparseArray<View>(mTitles.size());
-        }
+					mTabActionbar.onScrollTo(y);
+				} catch (NotSupportException e) {
+					Log.e(TAG, e.getMessage(), e);
+				}
+			}
+		});
+		return lv;
+	}
 
-        @Override
-        public int getCount() {
-            return mTitles.size();
-        }
+	private class MPagerAdapter extends PagerAdapter {
+		private ArrayList<CharSequence> mTitles = new ArrayList<CharSequence>(0);
+		private SparseArray<View> mViews;
 
-        public View getmViews(int pos) {
-            return mViews.get(pos);
-        }
+		public MPagerAdapter(ArrayList<CharSequence> titles) {
+			mTitles = titles;
+			mViews = new SparseArray<View>(mTitles.size());
+		}
 
-        @Override
-        public boolean isViewFromObject(View arg0, Object arg1) {
-            return arg0 == arg1;
-        }
+		@Override
+		public int getCount() {
+			return mTitles.size();
+		}
 
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            View v = getPageListView(mTitles.get(position), (position + 1) * 3);
+		public View getmViews(int pos) {
+			return mViews.get(pos);
+		}
 
-            container.addView(v);
-            mViews.put(position, v);
-            return v;
-        }
+		@Override
+		public boolean isViewFromObject(View arg0, Object arg1) {
+			return arg0 == arg1;
+		}
 
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            container.removeView(mViews.get(position));
-            mViews.remove(position);
-        }
+		@Override
+		public Object instantiateItem(ViewGroup container, int position) {
+			View v = getPageListView(mTitles.get(position), (position + 1) * 3);
 
-    }
+			container.addView(v);
+			mViews.put(position, v);
+			return v;
+		}
 
-    private class MTmpListAdapter extends BaseAdapter {
-        ArrayList<String> datas = new ArrayList<String>();
+		@Override
+		public void destroyItem(ViewGroup container, int position, Object object) {
+			container.removeView(mViews.get(position));
+			mViews.remove(position);
+		}
 
-        public MTmpListAdapter(CharSequence title, int count) {
-            for (int i = 0; i < count; i++) {
-                datas.add(title + " -> " + i);
-            }
-        }
+	}
 
-        @Override
-        public int getCount() {
-            return datas.size();
-        }
+	private class MTmpListAdapter extends BaseAdapter {
+		ArrayList<String> datas = new ArrayList<String>();
 
-        @Override
-        public Object getItem(int position) {
-            return datas.get(position);
-        }
+		public MTmpListAdapter(CharSequence title, int count) {
+			for (int i = 0; i < count; i++) {
+				datas.add(title + " -> " + i);
+			}
+		}
 
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
+		@Override
+		public int getCount() {
+			return datas.size();
+		}
 
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            TextView tvItem;
-            if (convertView == null) {
-                tvItem = (TextView) getLayoutInflater().inflate(android.R.layout.simple_list_item_1, parent, false);
-            } else {
-                tvItem = (TextView) convertView;
-            }
+		@Override
+		public Object getItem(int position) {
+			return datas.get(position);
+		}
 
-            tvItem.setText(datas.get(position));
-            return tvItem;
-        }
-    }
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			TextView tvItem;
+			if (convertView == null) {
+				tvItem = (TextView) getLayoutInflater().inflate(android.R.layout.simple_list_item_1, parent, false);
+			} else {
+				tvItem = (TextView) convertView;
+			}
+
+			tvItem.setText(datas.get(position));
+			return tvItem;
+		}
+	}
 }
--- src/com/caiyi/ui/MulTabActionBar.java
+++ src/com/caiyi/ui/MulTabActionBar.java
@@ -8,12 +8,8 @@ import android.os.Build;
 import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.view.GestureDetector;
-import android.view.GestureDetector.OnGestureListener;
 import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ImageView.ScaleType;
@@ -24,219 +20,159 @@ import com.nineoldandroids.animation.ObjectAnimator;
 import com.nineoldandroids.animation.ValueAnimator;
 import com.nineoldandroids.animation.ValueAnimator.AnimatorUpdateListener;
 
-public class MulTabActionBar extends LinearLayout implements OnGestureListener {
+public class MulTabActionBar extends LinearLayout {
 
-    private int mActionBarHeight;
-    private int mSwitchTabHeight;
-    /** 滚动状态0代表未滚动，1代表向下滚动(查看下面内容)，-1向上滚动(查看上面内容) **/
-    private int mScrollState = 0;
-    private Toolbar mActionBar;
-    private SwitchTitle mSwitchTitle;
-    private boolean mCanHideOnScroll;
+	private int mViewHeight;
+	private int mSwitchTabHeight;
+	/** 滚动状态0代表未滚动，1代表向下滚动(查看下面内容)，-1向上滚动(查看上面内容) **/
+	private int mScrollState = 0;
+	private Toolbar mActionBar;
+	private SwitchTitle mSwitchTitle;
+	private boolean mCanHideOnScroll;
 
-    private int mLastY;
-    private int mBackgoundColor;
+	private int mLastY;
+	private int mBackgoundColor;
 
-    GestureDetector mGestureDetector;
-    public static final int ANIMATION_DURATION = 300;
-    public static final String TAG = "MulTabActionBar";
-    boolean hasSetTouchuHideView = false;
+	public static final int ANIMATION_DURATION = 300;
+	public static final String TAG = "MulTabActionBar";
 
-    @TargetApi(Build.VERSION_CODES.L)
-    public MulTabActionBar(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
-        init(context, attrs, defStyleAttr, defStyleRes);
-    }
+	@TargetApi(Build.VERSION_CODES.L)
+	public MulTabActionBar(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+		super(context, attrs, defStyleAttr, defStyleRes);
+		init(context, attrs, defStyleAttr, defStyleRes);
+	}
 
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    public MulTabActionBar(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        init(context, attrs, defStyleAttr, 0);
-    }
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	public MulTabActionBar(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		init(context, attrs, defStyleAttr, 0);
+	}
 
-    public MulTabActionBar(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        init(context, attrs, 0, 0);
-    }
+	public MulTabActionBar(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init(context, attrs, 0, 0);
+	}
 
-    public MulTabActionBar(Context context) {
-        this(context, null);
-    }
+	public MulTabActionBar(Context context) {
+		this(context, null);
+	}
 
-    /** @return 获取toolbar对象 **/
-    public Toolbar getToolBar() {
-        return mActionBar;
-    }
+	/** @return 获取toolbar对象 **/
+	public Toolbar getToolBar() {
+		return mActionBar;
+	}
 
-    /** @return 获取switchTitle对象 **/
-    public SwitchTitle getSwitchTitle() {
-        return mSwitchTitle;
-    }
+	/** @return 获取switchTitle对象 **/
+	public SwitchTitle getSwitchTitle() {
+		return mSwitchTitle;
+	}
 
-    /** 根据可以滚动的最大距离判断是否可以隐藏actionBar **/
-    public void setCanHideOnScroll(int maxScroll) {
-        setCanHideOnScroll(maxScroll > mActionBarHeight);
-    }
+	/** 根据可以滚动的最大距离判断是否可以隐藏actionBar **/
+	public void setCanHideOnScroll(int maxScrollDelta) {
+		Log.d(TAG, "maxScroll=" + maxScrollDelta);
+		setCanHideOnScroll(maxScrollDelta > mViewHeight);
+	}
 
-    /** 设置是否可以根据滚动状态隐藏actionBar **/
-    public void setCanHideOnScroll(boolean canHide) {
-        if (mCanHideOnScroll == canHide) {
-            return;
-        }
-        mCanHideOnScroll = canHide;
-        runHeightAnimation(getLayoutParams(), mActionBarHeight);
-    }
+	/** 设置是否可以根据滚动状态隐藏actionBar **/
+	public void setCanHideOnScroll(boolean canHide) {
+		Log.d(TAG, "setCanHideOnScroll " + canHide);
 
-    public void onPageChanged(int scrollPos) {
-        mLastY = scrollPos;
-    }
+		if (mCanHideOnScroll == canHide) {
+			return;
+		}
+		mCanHideOnScroll = canHide;
+		runHeightAnimation(getLayoutParams(), mViewHeight);
+	}
 
-    /** 滑动动作触发 **/
-    public void onScrollTo(int nowY) {
-        if (!mCanHideOnScroll || hasSetTouchuHideView) {
-            return;
-        }
-        ViewGroup.LayoutParams lp = getLayoutParams();
-        int lastH = lp.height;
-        if (nowY == mLastY || (nowY < mLastY && lastH == mActionBarHeight)
-                || (nowY > mLastY && lastH == mSwitchTabHeight)) {
-            return;
-        }
+	public void onPageChanged(int scrollPos) {
+		mLastY = scrollPos;
+		ViewGroup.LayoutParams lp = getLayoutParams();
+		if (lp.height != mViewHeight) {
+			runHeightAnimation(getLayoutParams(), mViewHeight);
+		}
+	}
 
-        lp.height = Math.min(mActionBarHeight, Math.max(mSwitchTabHeight, lastH + mLastY - nowY));
+	/** 滑动动作触发 **/
+	public void onScrollTo(int nowY) {
+		if (!mCanHideOnScroll) {
+			return;
+		}
 
-//        Log.d(TAG, "scrollPos=" + scrollPos + "; height=" + lp.height);
+		ViewGroup.LayoutParams lp = getLayoutParams();
+		int lastH = lp.height;
+		if (nowY == mLastY || (nowY < mLastY && lastH == mViewHeight) || (nowY > mLastY && lastH == mSwitchTabHeight)) {
+			return;
+		}
 
-        mScrollState = nowY > mLastY ? 1 : -1;
-        mLastY = nowY;
-        setLayoutParams(lp);
-    }
+		lp.height = Math.min(mViewHeight, Math.max(mSwitchTabHeight, lastH + (mLastY - nowY) / 2));
 
-    /** actionUp动作触发，结束actionBar高度改变 **/
-    public void finishScroll() {
-        if (!mCanHideOnScroll) {
-            return;
-        }
-        ViewGroup.LayoutParams lp = getLayoutParams();
-        if (mScrollState == 1 && lp.height != -mActionBarHeight) {
-            runHeightAnimation(lp, mSwitchTabHeight);
-        } else if (mScrollState == -1 && lp.height != 0) {
-            runHeightAnimation(lp, mActionBarHeight);
-        }
-    }
+		Log.d(TAG, "nowY=" + nowY + "; height=" + lp.height);
 
-    /** 改变actionBar高度到指定值 **/
-    private void runHeightAnimation(final ViewGroup.LayoutParams lp, int to) {
+		mScrollState = nowY > mLastY ? 1 : -1;
+		mLastY = nowY;
+		setLayoutParams(lp);
+	}
+
+	/** actionUp动作触发，结束actionBar高度改变 **/
+	public void finishScroll() {
+		if (!mCanHideOnScroll) {
+			return;
+		}
+		ViewGroup.LayoutParams lp = getLayoutParams();
+		if (mScrollState == 1 && lp.height != -mViewHeight) {
+			runHeightAnimation(lp, mSwitchTabHeight);
+		} else if (mScrollState == -1 && lp.height != 0) {
+			runHeightAnimation(lp, mViewHeight);
+		}
+	}
+
+	/** 改变actionBar高度到指定值 **/
+	private void runHeightAnimation(final ViewGroup.LayoutParams lp, int to) {
 //        Log.d(TAG, "runHeightAnimation from=" + lp.height + "; to=" + to);
 
-        if (mActionBar.getTag() != null) {
-            ((ValueAnimator) mActionBar.getTag()).cancel();
-        }
+		if (mActionBar.getTag() != null) {
+			((ValueAnimator) mActionBar.getTag()).cancel();
+		}
 
-        ValueAnimator oa = ObjectAnimator.ofInt(lp.height, to);
-        oa.setDuration(ANIMATION_DURATION);
-        oa.addUpdateListener(new AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                lp.height = (int) animation.getAnimatedValue();
-                setLayoutParams(lp);
-            }
-        });
-        mActionBar.setTag(oa);
-        oa.start();
-    }
+		ValueAnimator oa = ObjectAnimator.ofInt(lp.height, to);
+		oa.setDuration(ANIMATION_DURATION);
+		oa.addUpdateListener(new AnimatorUpdateListener() {
+			@Override
+			public void onAnimationUpdate(ValueAnimator animation) {
+				lp.height = (Integer) animation.getAnimatedValue();
+				setLayoutParams(lp);
+			}
+		});
+		mActionBar.setTag(oa);
+		oa.start();
+	}
 
-    private void init(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        if (isInEditMode()) {
-            return;
-        }
-        setGravity(Gravity.BOTTOM);
-        setOrientation(VERTICAL);
-        Resources res = getResources();
-        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.mulTabActionBar, 0, 0);
-        mSwitchTabHeight = a.getDimensionPixelSize(R.styleable.mulTabActionBar_mtabTabHeight,
-                res.getDimensionPixelSize(R.dimen.switch_title_height));
-        mBackgoundColor = a.getColor(R.styleable.mulTabActionBar_mtabBackground, res.getColor(R.color.green_500));
-        a.recycle();
+	private void init(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+		if (isInEditMode()) {
+			return;
+		}
+		setGravity(Gravity.BOTTOM);
+		setOrientation(VERTICAL);
+		Resources res = getResources();
+		TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.mulTabActionBar, 0, 0);
+		mSwitchTabHeight = a.getDimensionPixelSize(R.styleable.mulTabActionBar_mtabTabHeight,
+				res.getDimensionPixelSize(R.dimen.switch_title_height));
+		mBackgoundColor = a.getColor(R.styleable.mulTabActionBar_mtabBackground, res.getColor(R.color.green_500));
+		a.recycle();
 
-        // 直接new Toolbar(...) 有很多麻烦的样式问题，故写在xml中了
-        mActionBar = (Toolbar) LayoutInflater.from(context).inflate(R.layout.action_bar, this, false);
-        mActionBar.setBackgroundColor(mBackgoundColor);
-        mActionBarHeight = res.getDimensionPixelSize(R.dimen.actionbar_height) + mSwitchTabHeight;
+		// 直接new Toolbar(...) 有很多麻烦的样式问题，故写在xml中了
+		mActionBar = (Toolbar) LayoutInflater.from(context).inflate(R.layout.action_bar, this, false);
+		mActionBar.setBackgroundColor(mBackgoundColor);
+		mViewHeight = res.getDimensionPixelSize(R.dimen.actionbar_height) + mSwitchTabHeight;
 
-        mSwitchTitle = new SwitchTitle(context, attrs, defStyleAttr);
-        mSwitchTitle.setBackgroundColor(mBackgoundColor);
-        ImageView ivShadow = new ImageView(context, attrs, defStyleAttr);
-        ivShadow.setScaleType(ScaleType.FIT_XY);
+		mSwitchTitle = new SwitchTitle(context, attrs, defStyleAttr);
+		mSwitchTitle.setBackgroundColor(mBackgoundColor);
+		ImageView ivShadow = new ImageView(context, attrs, defStyleAttr);
+		ivShadow.setScaleType(ScaleType.FIT_XY);
 
-        addView(mActionBar);
-        addView(mSwitchTitle, new LayoutParams(LayoutParams.MATCH_PARENT, mSwitchTabHeight));
+		addView(mActionBar);
+		addView(mSwitchTitle, new LayoutParams(LayoutParams.MATCH_PARENT, mSwitchTabHeight));
 
-        mGestureDetector = new GestureDetector(getContext(), this);
-    }
+	}
 
-    public void setTouchHideView(View v) {
-        if (v == null) {
-            hasSetTouchuHideView = false;
-            return;
-        }
-
-        hasSetTouchuHideView = true;
-        if (v instanceof ViewGroup) {
-            ((ViewGroup) v).setDescendantFocusability(ViewGroup.FOCUS_BEFORE_DESCENDANTS);
-        }
-        v.setOnTouchListener(new OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (mCanHideOnScroll) {
-                    if (event.getAction() == MotionEvent.ACTION_UP) {
-                        finishScroll();
-                    } else {
-                        mGestureDetector.onTouchEvent(event);
-                    }
-                }
-                return false;
-            }
-        });
-    }
-
-    @Override
-    public boolean onDown(MotionEvent e) {
-        return false;
-    }
-
-    @Override
-    public void onShowPress(MotionEvent e) {
-    }
-
-    @Override
-    public boolean onSingleTapUp(MotionEvent e) {
-        return false;
-    }
-
-    @Override
-    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-        if (Math.abs(distanceY) > Math.abs(distanceX)) {
-            Log.d(TAG, "onScroll -> distanceY=" + distanceY);
-
-            ViewGroup.LayoutParams lp = getLayoutParams();
-            int lastH = lp.height;
-            if ((distanceY > 0 && lastH == mSwitchTabHeight) || (distanceY < 0 && lastH == mActionBarHeight)) {
-                return false;
-            }
-            lp.height = Math.min(mActionBarHeight, Math.max(mSwitchTabHeight, (int) (lastH - distanceY)));
-            mScrollState = distanceY > 0 ? 1 : -1;
-            setLayoutParams(lp);
-        }
-        return false;
-    }
-
-    @Override
-    public void onLongPress(MotionEvent e) {
-    }
-
-    @Override
-    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-        return false;
-    }
 }
